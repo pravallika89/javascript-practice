51518d8f88a664bec579d53b14eb024f3e460343
pravallika <pravallikaammineni@github.com>
2020-11-08 21:13:20 -0800

    committed day 2 daily challenge

---
+++ JS/script.js
@@ -0,0 +1,14 @@
+
+let txt = "Supercalifragilisticexpialidocious";
+const number=45;
+
+console.log(txt.length)
+console.log(number)
+console.log(BigInt(9007199254740991))
+
+
+
+document.getElementById("demo").innerHTML = 
+typeof txt + "<br>" + 
+typeof number+"<br>" +
+typeof BigInt(9007199254740991);
